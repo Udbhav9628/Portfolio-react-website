2786224de52344cbb94eac14c0b7ef7c3b91533c
Udbhav9628 <udbhav9628@gmail.com>
2023-04-15 20:03:47 +0530

    Portfolio site

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,24 @@
 import '../style.css'
+import Navbar from './Navbar'
 
 const Hero = () => {
     return (
-        <div className='heroContainer'>Hero</div>
+        <div className='heroContainer'>
+            <Navbar />
+            <div className="mainContent">
+                <div id="mainContentLeft">
+                    <h1>Think. Make. Solve.</h1>
+                    <div id="whatweDo">
+                        <img src="" alt="" />
+                    </div>
+                    <div id="discription">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, officia.</p>
+                        <button>Hello</button>
+                    </div>
+                </div>
+                <div id="mainContentRight">Right</div>
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import '../style.css'
+const Logo = require('../img/logo.png')
+const searchIcon = require('../img/search.png')
+
+const Navbar = () => {
+    return (
+        <div className='navbarStyle'>
+            <div className='navContainer'>
+                <div className='navLinks'>
+                    <div>
+                        <img id='Logo' src={Logo} alt='Logo' />
+                    </div>
+                    <ul className='List'>
+                        <li className='ListItem'>Home</li>
+                        <li className='ListItem'>About</li>
+                        <li className='ListItem'>Works</li>
+                        <li className='ListItem'>Contact</li>
+                    </ul>
+                </div>
+                <div className='navIcons'>
+                    <img id='navSearchbar' src={searchIcon} alt='Search' />
+                    <button id='hireButton'>Hire Now</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
